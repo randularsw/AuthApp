9135b37216210dbad918a1c9235905bb093b87cd
Randula Wickramasuriya <randularsw@gmail.com>
2020-05-09 10:20:09 +0530

    initial commit

---
+++ auth-backend/app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+const port = 3002;
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.listen(port, () =>
+  console.log(`AuthApp listening at http://localhost:${port}`)
+);
+
+module.exports = app;
